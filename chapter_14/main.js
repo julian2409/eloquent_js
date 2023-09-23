806b98de7bbd702d659a725f540ac3bc77508557
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-09-23 15:02:03 +0200

    Continuing playing with the DOM...

--- chapter_14/main.js
+++ chapter_14/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
-{
-  let element = document.body.getElementsByTagName("h1")[0];
-
-  console.log(element);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.body.getElementsByTagName("p");
+  const element = elements[0];
+  console.log(element.innerHTML);
+  console.log(element.innerText);
+  console.log(element.textContent);
+});
