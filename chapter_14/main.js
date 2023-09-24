96e09facea9323244be40269060823b7d5dd9893
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-09-24 17:45:04 +0200

    Read chapter 14 and understood cat animation

--- chapter_14/main.js
+++ chapter_14/main.js
@@ -6,4 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log(element.innerHTML);
   console.log(element.innerText);
   console.log(element.textContent);
+
+  let cat = document.querySelector("img");
+  let angle = (3 * Math.PI) / 2;
+
+  function animate(time, lastTime) {
+    if (lastTime != null) {
+      angle += (time - lastTime) * 0.001;
+    }
+    cat.style.top = Math.sin(angle) * 20 + "px";
+    cat.style.left = Math.cos(angle) * 200 + "px";
+    requestAnimationFrame((newTime) => animate(newTime, time));
+  }
+  requestAnimationFrame(animate);
 });
