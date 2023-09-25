4910d30b4a70f1c9507b84267b2fa5bcb3b70d53
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-09-25 11:12:31 +0200

    Completed exercise 14/1

--- chapter_14/table.js
+++ chapter_14/table.js
@@ -2,13 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const MOUNTAINS = [
-    {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
-    {name: "Everest", height: 8848, place: "Nepal"},
-    {name: "Mount Fuji", height: 3776, place: "Japan"},
-    {name: "Vaalserberg", height: 323, place: "Netherlands"},
-    {name: "Denali", height: 6168, place: "United States"},
-    {name: "Popocatepetl", height: 5465, place: "Mexico"},
-    {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+    { name: "Kilimanjaro", height: 5895, place: "Tanzania" },
+    { name: "Everest", height: 8848, place: "Nepal" },
+    { name: "Mount Fuji", height: 3776, place: "Japan" },
+    { name: "Vaalserberg", height: 323, place: "Netherlands" },
+    { name: "Denali", height: 6168, place: "United States" },
+    { name: "Popocatepetl", height: 5465, place: "Mexico" },
+    { name: "Mont Blanc", height: 4808, place: "Italy/France" },
   ];
 
   const tableDiv = document.getElementById("mountains");
@@ -19,10 +19,35 @@ document.addEventListener("DOMContentLoaded", function () {
   const tableHeaderHeight = document.createElement("th");
   const tableHeaderPlace = document.createElement("th");
 
-  tableHeaderRow.appendChild(tableHeaderName);
-  tableHeaderRow.appendChild(tableHeaderHeight);
-  tableHeaderRow.appendChild(tableHeaderPlace);
+  tableHeaderRow
+    .appendChild(tableHeaderName)
+    .appendChild(document.createTextNode("Name"));
+  tableHeaderRow
+    .appendChild(tableHeaderHeight)
+    .appendChild(document.createTextNode("Height"));
+  tableHeaderRow
+    .appendChild(tableHeaderPlace)
+    .appendChild(document.createTextNode("Place"));
+
   table.appendChild(tableHeaderRow);
 
   tableDiv.appendChild(table);
+
+  for (let mountain of MOUNTAINS) {
+    let { name, height, place } = mountain;
+    const newTableRow = document.createElement("tr");
+    newTableRow
+      .appendChild(document.createElement("td"))
+      .appendChild(document.createTextNode(name));
+    newTableRow
+      .appendChild(document.createElement("td"))
+      .appendChild(document.createTextNode(height));
+    newTableRow
+      .appendChild(document.createElement("td"))
+      .appendChild(document.createTextNode(place));
+
+    newTableRow.childNodes[1].style.textAlign = "right";
+
+    table.appendChild(newTableRow);
+  }
 });
