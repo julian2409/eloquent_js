afab61f8e35e9dd0f3b62549a9ebcc4e991cff16
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-09-27 19:09:18 +0200

    Chapter 15 exercise 1 completed

---
+++ chapter_15/balloon.js
@@ -0,0 +1,41 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let balloonDiv = document.querySelector(".balloon");
+  balloonDiv.style.fontSize = "20px"
+
+  function reposition() {
+    const viewportHeight = window.innerHeight / 2;
+    balloonDiv.style.marginTop =
+      viewportHeight - balloonDiv.clientHeight / 2 + "px";
+  }
+  reposition();
+
+  visualViewport.addEventListener("resize", reposition);
+
+  function decreaseSize(event) {
+    if (event.key == "ArrowDown") {
+      const currentSize = parseFloat(balloonDiv.style.fontSize);
+      const newSize = currentSize - (currentSize / 10);
+      balloonDiv.style.fontSize = newSize + "px";
+      reposition();
+    }
+  }
+
+  function increaseSize(event) {
+    if (event.key == "ArrowUp") {
+      const currentSize = parseFloat(balloonDiv.style.fontSize);
+      if (currentSize > 200) {
+        balloonDiv.textContent = "💥";
+        this.window.removeEventListener("keyup", increaseSize);
+        this.window.removeEventListener("keyup", decreaseSize);
+      }
+      const newSize = currentSize + (currentSize / 10);
+      balloonDiv.style.fontSize = newSize + "px";
+      reposition();
+    }
+  }
+
+  window.addEventListener("keyup", increaseSize);
+  window.addEventListener("keyup", decreaseSize);
+});
