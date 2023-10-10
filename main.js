7d5f14acf74c0afb6f357ac4f4e2a50a8bce2bd1
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-10-10 23:07:21 +0200

    Starting chapter 16

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ myObject["testValue"] = 6;
 console.log(Object.prototype.hasOwnProperty.call(myObject, "testValue"));
 
 console.log(myObject.testValue);
+
+const someStrings = ["hello", "beautiful", "world"];
+
+const mappedStrings = someStrings.map((element, index) => {
+  console.log(`Processing element "${element}" at index ${index}`);
+  return [...element];
+});
+
+console.log(mappedStrings);
