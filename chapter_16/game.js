734964deced5c7de5481a59efe96efacef5f9aae
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-10-12 19:48:37 +0200

    Continuing...

--- chapter_16/game.js
+++ chapter_16/game.js
@@ -125,3 +125,33 @@ const levelChars = {
 
 let simpleLevel = new Level(simpleLevelPlan);
 console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+const scale = 20;
+
+function drawGrid(level) {
+  return elt(
+    "table",
+    {
+      class: "background",
+      style: `width: ${level.width * scale}px`,
+    },
+    ...level.rows.map((row) =>
+      elt(
+        "tr",
+        { style: `height: ${scale}px` },
+        ...row.map((type) => elt("td", { class: type }))
+      )
+    )
+  );
+}
