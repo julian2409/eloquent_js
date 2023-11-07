02347cc4ef0ed742368f49b2578072affcec7181
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-11-07 21:02:48 +0100

    Finished content chapter 17

--- chapter_17/game.js
+++ chapter_17/game.js
@@ -522,3 +522,101 @@ CanvasDisplay.prototype.clearDisplay = function (status) {
   }
   this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 };
+
+let otherSprites = document.createElement("img");
+otherSprites.src = "./assets/sprites.png";
+
+CanvasDisplay.prototype.drawBackGround = function (level) {
+  let { left, top, width, height } = this.viewport;
+  let xStart = Math.floor(left);
+  let xEnd = Math.ceil(left + width);
+  let yStart = Math.floor(top);
+  let yEnd = Math.ceil(top + height);
+
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      let tile = level.rows[y][x];
+      if (tile == "empty") continue;
+      let screenX = (x - left) * scale;
+      let screenY = (y - top) * scale;
+      let tileX = tile == "lava" ? scale : 0;
+      this.cx.drawImage(
+        otherSprites,
+        tileX,
+        0,
+        scale,
+        scale,
+        screenX,
+        screenY,
+        scale,
+        scale
+      );
+    }
+  }
+};
+
+let playerSprites = document.createElement("img");
+playerSprites.src = "./assets/player.png";
+const playerXOverlap = 4;
+
+CanvasDisplay.prototype.drawPlayer = function (player, x, y, width, height) {
+  width += playerXOverlap * 2;
+  x -= playerXOverlap;
+  if (player.speed.x != 0) {
+    this.flipPlayer = player.speed.x < 0;
+  }
+  let tile = 8;
+  if (player.speed.y != 0) {
+    tile = 9;
+  } else if (player.speed.x != 0) {
+    tile = Math.floor(Date.now() / 60) % 8;
+  }
+  this.cx.save();
+  if (this.flipPlayer) {
+    flipHorizontally(this.cx, x + width / 2);
+  }
+  let tileX = tile * width;
+  this.cx.drawImage(
+    playerSprites,
+    tileX,
+    0,
+    width,
+    height,
+    x,
+    y,
+    width,
+    height
+  );
+  this.cx.restore();
+};
+
+function flipHorizontally(context, around) {
+  context.translate(around, 0);
+  context.scale(-1, 1);
+  context.translate(-around, 0);
+}
+
+CanvasDisplay.prototype.drawActors = function (actors) {
+  for (let actor of actors) {
+    let width = actor.size.x * scale;
+    let height = actor.size.y * scale;
+    let x = (actor.pos.x - this.viewport.left) * scale;
+    let y = (actor.pos.y - this.viewport.top) * scale;
+    if (actor.type == "player") {
+      this.drawPlayer(actor, x, y, width, height);
+    } else {
+      let tileX = (actor.type == "coin" ? 2 : 1) * scale;
+      this.cx.drawImage(
+        otherSprites,
+        tileX,
+        0,
+        width,
+        height,
+        x,
+        y,
+        width,
+        height
+      );
+    }
+  }
+};
