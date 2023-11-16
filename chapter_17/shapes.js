a497f2aa835302d48d2dd8c40eff59a85d815714
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-11-16 22:55:24 +0100

    Continuing

--- chapter_17/shapes.js
+++ chapter_17/shapes.js
@@ -1,11 +1,11 @@
 "use strict";
 
+/** @type {CanvasRenderingContext2D} */
 let cx = document.querySelector(".myCanvas").getContext("2d");
 
 function drawTrapezoid(x, y) {
   cx.save();
   cx.translate(x, y);
-  cx.beginPath();
   cx.moveTo(0, 50);
   cx.lineTo(100, 50);
   cx.lineTo(80, 0);
@@ -31,25 +31,28 @@ function zigzag(x, y) {
   let nextX = width;
   cx.moveTo(0, 0);
   for (let y = 2.5; y < 80; y = y + 5, nextX = nextX + 75) {
-    cx.lineTo(nextX % (width*2), y);
+    cx.lineTo(nextX % (width * 2), y);
   }
   cx.restore();
 }
 
 function spiral(x, y) {
-  cx.save();
   cx.translate(x, y);
-  cx.moveTo(50, 50);
-  const increment = (6 * Math.PI) / 100;
-  for (let i = 0; i < 100; i++) {
-    console.log(increment);
+  let increment = (6 * Math.PI) / 100;
+  for (let i = 0; i < 100; i = i + 1) {
+    if (i % 30 == 0) {
+      cx.beginPath();
+      cx.arc(50, 0, 3 + increment * i, 0, Math.PI * 2);
+      cx.closePath();
+      cx.stroke();
+    }
   }
   cx.restore();
 }
 
+cx.beginPath();
 drawTrapezoid(5, 5);
 drawDiamond(150, 20);
 zigzag(250, 5);
-spiral(5, 150);
-
 cx.stroke();
+spiral(50, 200);
