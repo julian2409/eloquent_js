6149a10fe39d9bdec13b8d692dceb8cbb63ca6cb
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-11-19 01:48:12 +0100

    Files for exercise 17/3

---
+++ chapter_17/ball.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let cx = document.querySelector("canvas").getContext("2d");
+
+  let lastTime = null;
+  function frame(time) {
+    if (lastTime != null) {
+      updateAnimation(Math.min(100, time - lastTime) / 1000);
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+
+  function updateAnimation(step) {
+    // Your code here.
+  }
