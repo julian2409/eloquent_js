6132a279235647776724dffc2ad340b7ec29d59f
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-11-22 21:22:37 +0100

    Finished exercise 17/3 (there is a bug left though)

--- chapter_17/ball.js
+++ chapter_17/ball.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/** @type {CanvasRenderingContext2D} */
 let cx = document.querySelector("canvas").getContext("2d");
 
   let lastTime = null;
@@ -12,6 +13,26 @@ let cx = document.querySelector("canvas").getContext("2d");
   }
   requestAnimationFrame(frame);
 
+  const canvasX = 500, canvasY = 500;
+  let currentX = 200, currentY = 300;
+  let radius = 25;
+  let directionX = 1, directionY = 1;
+
   function updateAnimation(step) {
-    // Your code here.
+    cx.clearRect(currentX - radius - 5, currentY - radius - 5, radius * 2.3, radius * 2.3);
+    currentX = currentX + (step * 120 * directionX);
+    currentY = currentY + (step * 100 * directionY);
+
+    if (currentX > canvasX - radius || currentX - radius < 0) {
+      directionX = directionX * -1;
+    }
+
+    if (currentY > canvasY - radius || currentY - radius < 0) {
+      directionY = directionY * -1;
+    }
+
+    cx.beginPath();
+    cx.arc(currentX, currentY, radius, 0, 2 * Math.PI);
+    cx.fillStyle = "red";
+    cx.fill();
   }
